91349cfef4415759e2e940f50d5df2ec0d2f346c
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-01-05 04:15:02 +0000

    iii
    git-svn-id: svn://192.168.0.99/printurls@6563 5ada3c48-002d-0410-850d-99865cc6fd0f

--- printurls/FormInputUrl.Designer.cs
+++ printurls/FormInputUrl.Designer.cs
@@ -41,39 +41,62 @@
             // 
             // txtPageUrl
             // 
+            this.txtPageUrl.AccessibleDescription = null;
+            this.txtPageUrl.AccessibleName = null;
             resources.ApplyResources(this.txtPageUrl, "txtPageUrl");
+            this.txtPageUrl.BackgroundImage = null;
+            this.txtPageUrl.Font = null;
             this.txtPageUrl.Name = "txtPageUrl";
             // 
             // btnStart
             // 
+            this.btnStart.AccessibleDescription = null;
+            this.btnStart.AccessibleName = null;
             resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.BackgroundImage = null;
             this.btnStart.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnStart.Font = null;
             this.btnStart.Name = "btnStart";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnCancel
             // 
+            this.btnCancel.AccessibleDescription = null;
+            this.btnCancel.AccessibleName = null;
             resources.ApplyResources(this.btnCancel, "btnCancel");
+            this.btnCancel.BackgroundImage = null;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = null;
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
+            this.label1.AccessibleDescription = null;
+            this.label1.AccessibleName = null;
             resources.ApplyResources(this.label1, "label1");
+            this.label1.Font = null;
             this.label1.Name = "label1";
             // 
             // radioOpenUrl
             // 
+            this.radioOpenUrl.AccessibleDescription = null;
+            this.radioOpenUrl.AccessibleName = null;
             resources.ApplyResources(this.radioOpenUrl, "radioOpenUrl");
+            this.radioOpenUrl.BackgroundImage = null;
+            this.radioOpenUrl.Font = null;
             this.radioOpenUrl.Name = "radioOpenUrl";
             this.radioOpenUrl.UseVisualStyleBackColor = true;
             this.radioOpenUrl.CheckedChanged += new System.EventHandler(this.radioOpenUrl_CheckedChanged);
             // 
             // radioEnterURLs
             // 
+            this.radioEnterURLs.AccessibleDescription = null;
+            this.radioEnterURLs.AccessibleName = null;
             resources.ApplyResources(this.radioEnterURLs, "radioEnterURLs");
+            this.radioEnterURLs.BackgroundImage = null;
+            this.radioEnterURLs.Font = null;
             this.radioEnterURLs.Name = "radioEnterURLs";
             this.radioEnterURLs.UseVisualStyleBackColor = true;
             this.radioEnterURLs.CheckedChanged += new System.EventHandler(this.radioEnterURLs_CheckedChanged);
@@ -81,12 +104,20 @@
             // txtEnterURLs
             // 
             this.txtEnterURLs.AcceptsReturn = true;
+            this.txtEnterURLs.AccessibleDescription = null;
+            this.txtEnterURLs.AccessibleName = null;
             resources.ApplyResources(this.txtEnterURLs, "txtEnterURLs");
+            this.txtEnterURLs.BackgroundImage = null;
+            this.txtEnterURLs.Font = null;
             this.txtEnterURLs.Name = "txtEnterURLs";
             // 
             // btnExpand
             // 
+            this.btnExpand.AccessibleDescription = null;
+            this.btnExpand.AccessibleName = null;
             resources.ApplyResources(this.btnExpand, "btnExpand");
+            this.btnExpand.BackgroundImage = null;
+            this.btnExpand.Font = null;
             this.btnExpand.Name = "btnExpand";
             this.btnExpand.UseVisualStyleBackColor = true;
             this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
@@ -94,8 +125,11 @@
             // FormInputUrl
             // 
             this.AcceptButton = this.btnStart;
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
             this.CancelButton = this.btnCancel;
             this.Controls.Add(this.btnExpand);
             this.Controls.Add(this.txtEnterURLs);
@@ -105,6 +139,7 @@
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtPageUrl);
+            this.Font = null;
             this.Name = "FormInputUrl";
             this.Load += new System.EventHandler(this.FormInputUrl_Load);
             this.ResumeLayout(false);
